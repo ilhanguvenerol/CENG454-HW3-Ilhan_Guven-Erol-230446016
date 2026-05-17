90b2529f666e9ac1842a65b0039640818c5b4998
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-05-17 16:06:39 +0300

    connect decorator with player input

--- My project/Assets/Scripts/PlayerInput.cs
+++ My project/Assets/Scripts/PlayerInput.cs
@@ -6,10 +6,24 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private RhythmJudge rhythmJudge;
 
+    //plain base parry, replaced by decorators
+    private IParryAction _parryAction;
+
+    private void Awake()
+    {
+        _parryAction = new BaseParryAction();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             HandleParryInput();
+    }
+
+    //wrap the current action in a decorator
+    public void WrapParry(System.Func<IParryAction, IParryAction> decorator)
+    {
+        _parryAction = decorator(_parryAction);
     }
 
     private void HandleParryInput()
